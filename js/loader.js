8fc2cbcf078e00bfb0963c49c028ce47962b8464
LAVANYA SAXENA <lavanyasaxena0603@gmail.com>
2025-12-24 16:24:35 +0530

    Implement loading screen functionality

---
+++ js/loader.js
@@ -0,0 +1,40 @@
+// Loading Screen Controller
+let percent = 0;
+let loaderInterval;
+
+function startLoader() {
+    loaderInterval = setInterval(() => {
+        percent++;
+        document.getElementById("loader-percent").innerText = percent + "%";
+        document.getElementById("loader-bar").style.width = percent + "%";
+
+        if (percent >= 100) {
+            clearInterval(loaderInterval);
+            document.getElementById("loader").classList.add("fade-out");
+            setTimeout(() => {
+                document.getElementById("loader").style.display = "none";
+            }, 800);
+        }
+    }, 20);
+}
+
+// Allow click to skip
+document.addEventListener("click", function skipLoader() {
+    if (percent < 100) {
+        percent = 100;
+        document.getElementById("loader-percent").innerText = "100%";
+        document.getElementById("loader-bar").style.width = "100%";
+        clearInterval(loaderInterval);
+        document.getElementById("loader").classList.add("fade-out");
+        setTimeout(() => {
+            document.getElementById("loader").style.display = "none";
+        }, 800);
+    }
+});
+
+// Start loading when DOM is ready
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', startLoader);
+} else {
+    startLoader();
+}
